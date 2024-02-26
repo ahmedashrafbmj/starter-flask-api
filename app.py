73baeafe629f55c662b5f27fd5868148eed555a8
ahmedashrafbmj <ahmedain7557@gmail.com>
2024-02-26 17:55:08 +0500

    new

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
-from flask import Flask, render_template, jsonify, request
-from flask_cors import CORS
-import requests
 
-RASA_API_URL = 'http://localhost:5005/webhooks/rest/webhook'
-app = Flask(__name__,static_url_path='/static')
-CORS(app) 
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS from flask_cors
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+import joblib
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+app = Flask(__name__)
+CORS(app)  # Add CORS support to your Flask app
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    try:
-        user_message = request.json['message']
-        print(user_message)
-        
-        # Use requests.post to make the HTTP POST request
-        rasa_response = requests.post(RASA_API_URL, json={'message': user_message})
-        rasa_response_json = rasa_response.json()
+# Load and preprocess data
+data = pd.read_csv("Mental_Health_FAQ.csv", encoding='latin1')
+data['Questions'] = data['Questions'].str.lower()
+data['Questions'] = data['Questions'].str.replace('[^\w\s]', '')
+data.dropna(inplace=True)
 
-        # Assuming rasa_response_json is a list with text as one of its elements
-        bot_response = rasa_response_json[0]['text'] if rasa_response_json else 'Sorry, I don\'t understand that.'
-        return jsonify({'response': bot_response})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+# Train model and save
+vectorizer = TfidfVectorizer()
+X_train_vec = vectorizer.fit_transform(data['Questions'])
+model = LinearSVC()
+model.fit(X_train_vec, data['Answers'])
+joblib.dump(model, 'mental_health_intent_model.pkl')
+joblib.dump(vectorizer, 'tfidf_vectorizer.pkl')
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+
+# Predict intent
+@app.route('/predict', methods=['POST'])
+def predict_intent():
+    user_input = request.json['question']
+    user_input_vec = vectorizer.transform([user_input.lower()])
+    predicted_intent = model.predict(user_input_vec)[0]
+    response = predicted_intent  # Return the predicted intent for now, you may modify this to return the actual answer
+    return jsonify({'response': response})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
